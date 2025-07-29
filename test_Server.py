fde769020c776100b1b7b6f459aafc92b333d9e3
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-07-29 10:38:28 +0200

    fix

--- test_Server.py
+++ test_Server.py
@@ -28,9 +28,18 @@ def translate_text(sentence, src_lang="eng_Latn", tgt_lang="kan_Knda"):
 
 # Example usage
 if __name__ == "__main__":
+
+
+    file_name = "data/shrimad_bhagavatam_english.txt"
+    # Open the file in read mode
+    with open(file_name, 'r') as file:
+        file_content = file.read()
+
+    # Now file_content holds the entire content of the file as a string
+    print(file_content)
     
-    text = "hi"
-    result = translate_text(text)
+    #text = "hi"
+    result = translate_text(file_content)
 
 
 
